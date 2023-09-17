84b1a8428902c9c1c4b0d81cf967f3c78b235653
AgungSukaAFK <wismaagung23@gmail.com>
2023-09-17 21:33:35 +0700

    asdff

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const store = new MongoDBStore({
   collection: "mySessions",
 });
 
+app.set('views', path.join(__dirname, 'views'));
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
